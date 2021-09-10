0901f2b95fbe94ed173b812609e5af9d758a8e88
Carlos Lisandra Osborne <carlos.lisandra@toolfactory.net>
2021-09-10 14:06:07 +0200

    WIP

---
+++ samples/src/TodoSampleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+
+AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new TodoPlayground.Todo.TodoClient(channel);
+
+var createReply = await client.CreateTodoAsync(new TodoPlayground.CreateTodoRequest { IsComplete = false, Title = "My First Title" });
+
+Console.WriteLine("Created: {0}", createReply.Id);
+var searchByIdReply = await client.GetTodoByIdAsync(new TodoPlayground.TodoByIdRequest { Id = createReply.Id });
+Console.WriteLine(searchByIdReply.Title);
+
+var updateReply = await client.UpdateTodoAsync(new TodoPlayground.UpdateTodoRequest { Id = createReply.Id, Todo = new TodoPlayground.TodoRequest { Title = "My new title" } });
+var  searchByIdReplyAgain = await client.GetTodoByIdAsync(new TodoPlayground.TodoByIdRequest { Id = createReply.Id });
+Console.WriteLine(searchByIdReplyAgain.Title);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ src/MinimalServerPlayground/Program.cs
@@ -0,0 +1,132 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using TodoPlayground;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TodoDb") ?? "Data Source=todos.db";
+builder.Services.AddSqlite<TodoDb>(connectionString)
+                .AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+await EnsureDb(app.Services, app.Logger);
+
+app.MapGrpcService<TodoService>();
+app.Run();
+
+
+async Task EnsureDb(IServiceProvider services, ILogger logger)
+{
+    logger.LogInformation("Ensuring database exists and is up to date at connection string '{connectionString}'", connectionString);
+
+    using var db = services.CreateScope().ServiceProvider.GetRequiredService<TodoDb>();
+    await db.Database.MigrateAsync();
+}
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<TodoPlayground.TodoReply> Todos => Set<TodoPlayground.TodoReply>();
+}
+class TodoService : Todo.TodoBase
+{
+    private readonly TodoDb _db;
+
+    private static Status _notFoundStatus = new Status(StatusCode.NotFound, "Not found");
+    public TodoService(TodoDb database)
+    {
+        _db = database;
+    }
+
+    public override async Task<CreateTodoReply> CreateTodo(CreateTodoRequest request, ServerCallContext context)
+    {
+        var res = _db.Todos.Add(new TodoReply { IsComplete = request.IsComplete, Title = request.Title });
+        await _db.SaveChangesAsync();
+
+
+        return new CreateTodoReply
+        {
+            Id = res.Entity.Id
+        };
+
+
+    }
+
+    public override async Task<Empty> DeleteTodo(TodoByIdRequest request, ServerCallContext context)
+    {
+        if (await _db.Todos.FindAsync(request.Id) is TodoReply todo)
+        {
+            _db.Todos.Remove(todo);
+            await _db.SaveChangesAsync();
+            return new Empty();
+        }
+        throw new RpcException(_notFoundStatus);
+    }
+
+    public override async Task GetAllTodos(Empty request, IServerStreamWriter<TodoReply> responseStream, ServerCallContext context)
+    {
+        await foreach (var todo in _db.Todos.AsAsyncEnumerable())
+        {
+            await responseStream.WriteAsync(todo);
+        }
+    }
+
+    public override async Task GetCompleteTodos(Empty request, IServerStreamWriter<TodoReply> responseStream, ServerCallContext context)
+    {
+        await foreach (var todo in _db.Todos.Where(t => t.IsComplete).AsAsyncEnumerable())
+        {
+            await responseStream.WriteAsync(todo);
+        }
+    }
+
+    public override async Task GetIncompleteTodos(Empty request, IServerStreamWriter<TodoReply> responseStream, ServerCallContext context)
+    {
+        await foreach (var todo in _db.Todos.Where(t => !t.IsComplete).AsAsyncEnumerable())
+        {
+            await responseStream.WriteAsync(todo);
+        }
+    }
+
+    public override async Task<TodoReply> GetTodoById(TodoByIdRequest request, ServerCallContext context)
+    {
+        return await _db.Todos.FindAsync(request.Id) is TodoReply todo ? todo : new TodoReply();
+    }
+
+    public override async Task<UpdateTodoReply> MarkComplete(TodoByIdRequest request, ServerCallContext context)
+    {
+        var todo = await _db.Todos.FindAsync(request.Id);
+        if (todo is null) throw new RpcException(_notFoundStatus);
+
+        todo.IsComplete = true;
+        await _db.SaveChangesAsync();
+        return new();
+    }
+
+    public override async Task<UpdateTodoReply> MarkInComplete(TodoByIdRequest request, ServerCallContext context)
+    {
+        var todo = await _db.Todos.FindAsync(request.Id);
+        if (todo is null) throw new RpcException(_notFoundStatus);
+
+        todo.IsComplete = false;
+        await _db.SaveChangesAsync();
+        return new();
+    }
+
+    public override async Task<UpdateTodoReply> UpdateTodo(UpdateTodoRequest request, ServerCallContext context)
+    {
+        var todo = await _db.Todos.FindAsync(request.Id);
+        if (todo is null) throw new RpcException(_notFoundStatus);
+
+        todo.Title = request.Todo.Title;
+        todo.IsComplete = request.Todo.IsComplete;
+        await _db.SaveChangesAsync();
+        return new();
+    }
+
+}
